b3f125911dec96c0a4bf926f578ab5819aa808d3
Lakshya suthar <lakshyasuthar1910@gmail.com>
2024-11-21 15:08:30 +0530

    exp 6.c

--- exp 6.c
+++ exp 6.c
@@ -28,26 +28,4 @@ free(nextnode);
 
      }
 }
-int main() {
-    struct Node* root = NULL;  // Start with an empty BST
 
-    // Insert nodes into the BST
-    root = insert(root, 50);
-    root = insert(root, 30);
-    root = insert(root, 20);
-    root = insert(root, 40);
-    root = insert(root, 70);
-    root = insert(root, 60);
-    root = insert(root, 80);
-
-    
-    // Search for a value
-    int value = 40;
-    if (search(root, value)) {
-        printf("Node with value %d found in the BST.\n", value);
-    } else {
-        printf("Node with value %d not found in the BST.\n", value);
-    }
-
-    return 0;
-}
